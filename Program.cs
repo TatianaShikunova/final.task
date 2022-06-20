aaa418a5ba3787c256c7f5f67db2d0ff2b895eff
Tatiana <camellia.ts17@gmail.com>
2022-06-20 13:00:26 +0300

    FillArray

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество элементов массива строк:");
+int size = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите элементы массива строк:");
+string [] a = new string [size];
+FillArray(a);
+
+void FillArray(string [] array)
+{
+    for (int i = 0; i<array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+        
+    }
+}
